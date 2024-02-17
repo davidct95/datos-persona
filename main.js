3e4bab0354ee875865c3efb794e5273b5b83d06b
David <97263791+davidct95@users.noreply.github.com>
2024-02-17 01:24:22 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,29 @@
+fetch("http://localhost:8080/lista", {
+  method: "GET",
+  headers: {
+    "Content-Type": "application/json", // Ajusta los encabezados según lo necesites
+    // Puedes agregar otros encabezados aquí si es necesario, como tokens de autenticación
+  },
+})
+  .then((response) => {
+    if (!response.ok) {
+      throw new Error("Network response was not ok");
+    }
+    return response.json(); // Parsea la respuesta JSON
+  })
+  .then((data) => {
+    const tbody = document.querySelector("#tabla tbody");
+
+    data.forEach((item) => {
+      const fila = document.createElement("tr");
+      const nombre = item.nombre;
+      const celdaNombre = document.createElement("td");
+      celdaNombre.textContent = nombre;
+      fila.appendChild(celdaNombre);
+
+      tbody.appendChild(fila);
+    });
+  })
+  .catch((error) => {
+    console.error("There has been a problem with your fetch operation:", error);
+  });
